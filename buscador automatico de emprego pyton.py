0ddac4a96508c611e294974cb12d5b6d8463cd0e
Matheus-dev <matheusaugusto2029@gmail.com>
2026-04-27 13:19:49 -0300

    Add files via upload

---
+++ buscador automatico de emprego pyton.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+pyautogui.PAUSE=2
+pyautogui.press ("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+pyautogui.write("vagas de emprego de python")
+pyautogui.press("enter")
